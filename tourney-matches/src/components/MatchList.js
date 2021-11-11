9e25bbf7c0ffbce1c3e902d6796ced7aaf4eff06
LloydDub <lloydrww@gmail.com>
2021-11-10 20:59:14 -0700

    comments and the lehjekj

--- tourney-matches/src/components/MatchList.js
+++ tourney-matches/src/components/MatchList.js
@@ -3,7 +3,7 @@
 import React from "react"; //optional
 import Match from "./Match";
 
-function MatchList() {
+function MatchList(props) {
   return (
     <section className="PlayerList MatchList">
       <h1>Match list</h1>
--- tourney-matches/src/components/PlayerList.js
+++ tourney-matches/src/components/PlayerList.js
@@ -1,9 +1,14 @@
 // PlayerList.js
-
 import React from "react"; //optional
+import playerData from "../data/playerData";
+import matchData from "../data/matchData";
 import Player from "./Player";
 
-function PlayerList() {
+import { preparePlayerData, addWinsToPlayers } from "../helpers/playerHelpers";
+
+function PlayerList(props) {
+  const playerDataArray = preparePlayerData(playerData);
+  const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
   return (
     <section className="PlayerList">
       <h1>Current participating players</h1>
